d2d3cae29fc3b2aa3425385aea3c98044f86f46b
brunodamasco <brunodamasco1@gmail.com>
2019-05-07 01:31:36 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/bruno/cursomc/domain/Categoria.java
+++ src/main/java/com/bruno/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.bruno.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -15,6 +18,9 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 	}
@@ -41,30 +47,13 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+	public List<Produto> getProdutos() {
+		return produtos;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Categoria other = (Categoria) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
-	
+
 	
 }
